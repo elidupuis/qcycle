a441945b790df8466e7c5decb5e801c4b132b03c
Eli Dupuis <elidupuis@gmail.com>
2010-05-11 22:05:33 -0600

    v0.4 - allow images only to be passed in for simplest usage. create new demo. add onPageLoad option

--- jquery.qCycle.js
+++ jquery.qCycle.js
@@ -1,7 +1,7 @@
 /**
  *	jQuery qCycle plugin
- *	@version 0.3
- *	@date Nov 10, 2009
+ *	@version 0.4
+ *	@date April 11, 2010
  *	@author Eli Dupuis
  *	@license Creative Commons Share Alike 2.5 Canada (http://creativecommons.org/licenses/by-sa/2.5/ca/)
  *	Requires: jQuery v1.3.2 or later (most likely works fine with earlier versions, but untested)
@@ -9,16 +9,19 @@
  *				uses jquery.cycle.addSlide() function which is not supported in jquery.cycleLite (http://www.malsup.com/jquery/cycle/lite/)
  *
  *	CHANGLOG:
+ *	v0.4
+	-added ability to pass in an array of images only, as opposed to an array of objects containing image paths as well as other data. 
+	-updated start property to be a boolean called onPageLoad. by default the plugin waits for $(window).load. set to false to run immediately
  *	v0.3
 	-added 'start' option. can be either 'pageload' or 'immediate'. immediate is equivalent to domReady since you should not be calling qCycle before the DOM is ready. pageload waits for the rest of the page to load (fired on window.load)
  *
  *  TODO: support JSON import for data...
- *	TODO: add ability to pass in a simple array of image filepaths--no need for extra data, just images.
+ *	TODO: optimize!
 */
 
 (function($) {
 
-var ver = '0.3';
+var ver = '0.4';
 
 $.fn.qCycle = function(options) {
 
@@ -31,12 +34,12 @@ $.fn.qCycle = function(options) {
 		var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
 		$this.data('qCycle.opts',o);
 
-		if (opts.start == 'immediate') {
-			initSlideshow($this);
-		}else{
+		if (opts.onPageLoad) {
 			$(window).load(function(){
 				initSlideshow($this);
 			});
+		}else{
+			initSlideshow($this);			
 		};
 
 	});
@@ -47,14 +50,20 @@ function initSlideshow(obj){
 	obj.data( 'qCycle.slidesLoaded', 0 );
 	
 	var loaded = obj.data( 'qCycle.slidesLoaded' );
-
-	if (o.toLoad != null) $('<img/>').load(function(){ 
+	
+	if (o.toLoad != null) {
+		var theImage = $('<img/>').load(function(){ 
 			addSlide($(this),obj,true);
 		}).data(
 			'qCycle.slideData',o.toLoad[loaded]
-		).attr(
-			'src',o.toLoad[loaded].img
 		);
+		
+		if (o.toLoad[loaded].img) {
+			theImage.attr( 'src', o.toLoad[loaded].img );
+		}else{
+			theImage.attr( 'src', o.toLoad[loaded] );
+		};
+	}
 	
 	obj.data( 'qCycle.slidesLoaded', obj.data( 'qCycle.slidesLoaded' )+1 );
 	
@@ -75,16 +84,22 @@ function startSlideshow(obj){
 function loadSlide(obj){
 	var loaded = obj.data( 'qCycle.slidesLoaded' );
 	
-	$('<img/>').load(function(){ 
+	var theImage = $('<img/>').load(function(){ 
 			addSlide($(this),obj);
 		}).data(
 			'qCycle.slideData',obj.data('qCycle.opts').toLoad[loaded]
-		).attr(
-			'src',obj.data('qCycle.opts').toLoad[loaded].img
 		);
+		
+	if (obj.data('qCycle.opts').toLoad[loaded].img) {
+		theImage.attr( 'src', obj.data('qCycle.opts').toLoad[loaded].img );
+	}else{
+		theImage.attr( 'src', obj.data('qCycle.opts').toLoad[loaded] );
+	};
+	
 };
 
 function addSlide(img,obj,init){
+	if(window.console) window.console.log(img,obj,init);
 	
 	var cycleOpts = obj.data('qCycle.cycleOpts');
 
@@ -112,10 +127,10 @@ function addSlide(img,obj,init){
 // plugin defaults
 //
 $.fn.qCycle.defaults = {
-	toLoad: null,									//	array of objects. each object must have attribute 'img' that contains the url of an image to load.
-	cycleOpts:{},									//	passed directly into jQuery.cycle on init. see http://www.malsup.com/jquery/cycle/options.html for options.
-	createSlide: function(img){	return img;	},		//	function where slides are created. default just returns the loaded image.
-	start: 'pageload'								//	one of ['pageload', 'immediate']
+	toLoad: null,
+	cycleOpts:{},
+	createSlide: function(img){	return img;	},
+	onPageLoad: true
 };
 
 //	public function/method
